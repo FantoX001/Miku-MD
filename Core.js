77baf3563088452c32f640eb862159d2ac253cce
shibam00007 <100935103+shibam00007@users.noreply.github.com>
2022-11-14 02:48:42 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -3894,7 +3894,7 @@ if (!m.isGroup) return replay(mess.grouponly)
 if (!AntiNsfw) return reply(mess.nonsfw)
 try{
 reply(mess.waiting)
-NoHorny = await fetchJson(`https://api-reysekha.herokuapp.com/api/nsfw/${command}`)
+NoHorny = await fetchJson(`https://api-reysekha.herokuapp.com/api/nsfw/${command}?apikey=APIKEY`)
 YesHorny = await getBuffer(NoHorny.result)
 Miku.sendMessage(from, {image:YesHorny},{quoted:m})
 } catch (e) {error("Error")}	

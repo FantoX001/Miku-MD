ee589aacb250e1166ef5eb7cff029f7925627025
AshAritra <86512807+AshAritra@users.noreply.github.com>
2022-12-08 17:36:53 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -1,6 +1,5 @@
 process.on('uncaughtException', console.error)
 require("./config")
-require("./Query")
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType, WAFlag } = require('@adiwajshing/baileys')
 const zMiku = require("@adiwajshing/baileys")
 const fs = require('fs')
